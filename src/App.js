9f5b7535eb84daa16c5615fd72fa21bfb9c51702
Simon-Eriksson <127835440+Simon-Eriksson@users.noreply.github.com>
2024-09-12 09:33:24 +0200

    HighScoreModal

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
+import React, { useState } from 'react';
 import "./App.css";
 import Timer from './Timer';
+import HighScoreModal from "./HighScoreModal";
 
 function App() {
+
+  const [showModal, setShowModal] = useState(false);
+
+  // funktion som triggar när timer är 0
+  const handleGameOver = () => {
+    setShowModal(true);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>WHACK A MOLE</h1>
       </header>
 
-      <Timer />
+      {/* pass prop, timer */}
+      <Timer onGameOver={handleGameOver} />
+
+      {/* visar HighScoreModal när tiden är ute */}
+      <HighScoreModal
+        show={showModal}
+        onClose={() => setShowModal(false)}
+        onStartNewGame={() => setShowModal(false)}
+      />
 
     </div>
   );
