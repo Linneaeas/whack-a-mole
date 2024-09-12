4c45dcdde9eeb2054762175e52c3b330536b3ea4
Angelica Matilda <matildagothlund@hotmail.com>
2024-09-12 09:37:34 +0200

    Visual board

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Timer from './Timer';
+import Board from './Board'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       </header>
 
       <Timer />
+      <Board />
 
     </div>
   );
